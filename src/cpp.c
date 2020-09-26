acdb0414021c99e29d5b7a727c1734bc6732240b
Mate Kukri <kukri.mate@gmail.com>
2020-09-26 10:47:55 +0100

    Add escape sequences

--- src/cpp.c
+++ src/cpp.c
@@ -12,7 +12,7 @@ static
 void
 cpp_err(void)
 {
-	fprintf(stderr, "Invalid input!\n");
+	fprintf(stderr, "Lexer error!\n");
 	exit(1);
 }
 
@@ -22,13 +22,11 @@ identifier(struct chvec *v, int ch)
 {
 	chvec_add(v, ch);
 	for (;;) {
-		ch = mgetc();
-		chvec_add(v, ch);
-
-		switch (ch) {
+		switch (mpeek()) {
 		/* Identifier character */
 		NONDIGIT
 		DIGIT
+			chvec_add(v, mgetc());
 			break;
 		/* End of identifier */
 		default:
@@ -39,22 +37,131 @@ identifier(struct chvec *v, int ch)
 
 static
 void
+octal(struct chvec *v, int ch)
+{
+	ch -= '0';
+	/* Octal constants only allow 3 digits max */
+	switch (mpeek()) {
+	ODIGIT
+		ch <<= 3;
+		ch |= mgetc() - '0';
+		break;
+	default:
+		goto endc;
+	}
+	switch (mpeek()) {
+	ODIGIT
+		ch <<= 3;
+		ch |= mgetc() - '0';
+		break;
+	}
+endc:
+	chvec_add(v, ch);
+}
+
+static
+int
+hexdigit_to_int(int ch)
+{
+	if (ch >= '0' && ch <= '9')
+		return ch - '0';
+	/* FIXME: assuming a-z and A-Z being continous is non-standard */
+	if (ch >= 'a' && ch <= 'f')
+		return 10 + (ch - 'a');
+	if (ch >= 'A' && ch <= 'F')
+		return 10 + (ch - 'A');
+	/* Not a valid hex digit */
+	cpp_err();
+	return 0; /* Never reached, but makes gcc shut up */
+}
+
+static
+void
+hexadecimal(struct chvec *v)
+{
+	int ch;
+
+	ch = 0;
+	/* Hex constants can be any length */
+	for (;;)
+		switch (mpeek()) {
+		HDIGIT
+			ch <<= 4;
+			ch |= hexdigit_to_int(mgetc());
+			break;
+		default:
+			goto endloop;
+		}
+endloop:
+	chvec_add(v, ch);
+}
+
+static
+void
+escseq(struct chvec *v, int ch)
+{
+	switch (ch = mgetc()) {
+	case '\'':	/* Simple escape sequences */
+	case '"':
+	case '?':
+	case '\\':
+		chvec_add(v, ch);
+		break;
+	case 'a':
+		chvec_add(v, '\a');
+		break;
+	case 'b':
+		chvec_add(v, '\b');
+		break;
+	case 'f':
+		chvec_add(v, '\f');
+		break;
+	case 'n':
+		chvec_add(v, '\n');
+		break;
+	case 'r':
+		chvec_add(v, '\r');
+		break;
+	case 't':
+		chvec_add(v, '\t');
+		break;
+	case 'v':
+		chvec_add(v, '\v');
+		break;
+	ODIGIT		/* Octal */
+		octal(v, ch);
+		break;
+	case 'x':	/* Hexadecimal */
+		hexadecimal(v);
+		break;
+	/* Invalid escape sequence */
+	default:
+		cpp_err();
+	}
+}
+
+static
+void
 string(struct chvec *v, int ch)
 {
 	chvec_add(v, ch);
 	for (;;) {
 		ch = mgetc();
-		chvec_add(v, ch);
-
 		switch (ch) {
-		/* Unterminated string */
+		/* Normal character */
+		default:
+			chvec_add(v, ch);
+			if (ch == '"') /* End of string */
+				return;
+			break;
+		/* Escape sequence */
+		case '\\':
+			escseq(v, ch);
+			break;
+		/* Invalid string */
 		case EOF:
 		case '\n':
 			cpp_err();
-		/* FIXME: add escape sequences */
-		/* End of string */
-		case '"':
-			return;
 		}
 	}
 }
@@ -137,7 +244,7 @@ punctuator(struct chvec *v, int ch)
 			cpp_err();
 		break;
 	default: /* Invalid punctuator, this should never be able to happen,
-			unless the compiler the lexer is broken */
+			left here to sanity check for bugs */
 		cpp_err();
 	}
 
@@ -152,6 +259,21 @@ cpp_tokenize(void)
 
 	chvec_init(&v);
 	while ((ch = mgetc()) != EOF) {
+		if (ch == '/') {
+			/* New style comment */
+			if (mnext('/') == '/') {
+				while (ch = mgetc(),
+					ch != '\n' && ch != EOF);
+				continue;
+			}
+			/* Old style comment */
+			if (mnext('*') == '*') {
+				while (ch = mgetc(),
+					!(ch == '*' && mnext('/') == '/'));
+				continue;
+			}
+		}
+
 		switch (ch) {
 		/* Whitespace */
 		WHITESPACE
--- src/cpp.h
+++ src/cpp.h
@@ -20,10 +20,22 @@
 	case 'P':case 'Q':case 'R':case 'S':case 'T':case 'U':case 'V':\
 	case 'W':case 'X':case 'Y':case 'Z':
 
+/* Octal digits */
+#define ODIGIT \
+	case '0':case '1':case '2':case '3':case '4':case '5':case '6':\
+	case '7':
+
 /* Decimal digits */
 #define DIGIT \
 	case '0':case '1':case '2':case '3':case '4':case '5':case '6':\
 	case '7':case '8':case '9':
+
+/* Hexadecimal digits */
+#define HDIGIT \
+	case '0':case '1':case '2':case '3':case '4':case '5':case '6':\
+	case '7':case '8':case '9':case 'a':case 'b':case 'c':case 'd':\
+	case 'e':case 'f':case 'A':case 'B':case 'C':case 'D':case 'E':\
+	case 'F':
 
 /* Characters used to build punctuators */
 #define PUNCTUATOR \
